0bdffe41e7a523b949edcc923fc1aa2b833a297c
Vladimir Osin <vladimir.osin@signify.com>
2020-09-18 21:51:52 +0200

    update main dag in project 5

--- 5-data-pipelines-airflow/dags/udac_example_dag.py
+++ 5-data-pipelines-airflow/dags/udac_example_dag.py
@@ -11,8 +11,8 @@ default_args = {
     'owner': 'udacity',
     'start_date': datetime(2019, 1, 12),
     'depends_on_past': False,
-    # 'retries': 3,
-    # 'retry_delay': timedelta(minutes=5),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
     'catchup': False,
     'email_on_retry': False,
 
